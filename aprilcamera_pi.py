4752452eb5a2fa22ae2c55f1fa29aa99ad971f19
MedNourS <sabir.med.nour@gmail.com>
2023-12-01 17:31:08 -0500

    updated

--- aprilcamera_pi.py
+++ aprilcamera_pi.py
@@ -2,45 +2,46 @@ import cv2
 import numpy as np
 import robotpy_apriltag as rpat
 
-def detect_apriltags(frame):
-    # Convert the frame to grayscale
+def detect_and_estimate_poses(frame, estimator):
+    
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Create an AprilTag detector
     detector = rpat.AprilTagDetector()
     detector.addFamily("tag36h11")
-    # Detect AprilTags in the grayscale frame
+
     detections = detector.detect(gray_frame)
 
-    # Process each detection
     for detection in detections:
-        # Get tag information  
+
         tag_id = detection.getId()
         x, y = detection.getCenter().x, detection.getCenter().y
-        distance = detection.pose_error
 
-        # Print the information
-        print(f"[tag= {tag_id}, x= {x}, {y}, distance = {distance}]")
+        pose = estimator.estimate(detection)
+
+        translation = pose.translation()
+        rotation = pose.rotation()
+        roll, pitch, yaw = rotation.x_degrees, rotation.y_degrees, rotation.z_degrees
+
+        print("-----------------------------------------------------")    
+        print(f"tag = {tag_id}\nx = {x}, y = {y}\nTranslation = [{translation.x}, {translation.y}, {translation.z}]\nRotation = [{roll}, {pitch}, {yaw}]")
 
 def main():
-    # Open the camera (0 is usually the default camera)
     cap = cv2.VideoCapture(0)
+    
+    tag_size = 0.1778
+    fx, fy, cx, cy = 1000, 1000, 640, 480
+    estimator_config = rpat.AprilTagPoseEstimator.Config(tag_size, fx, fy, cx, cy)
+    estimator = rpat.AprilTagPoseEstimator(estimator_config)
 
     while True:
-        # Capture a frame from the camera
+
         ret, frame = cap.read()
 
-        # Detect AprilTags in the captured frame
-        detect_apriltags(frame)
+        detect_and_estimate_poses(frame, estimator)
 
-        # Display the frame
-        cv2.imshow("AprilTag Detection", frame)
-
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
-    # Release the camera and close the OpenCV window
     cap.release()
     cv2.destroyAllWindows()
 
